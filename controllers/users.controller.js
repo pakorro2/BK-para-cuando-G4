fad2f4384a12feff747cdf460b0abc72720f0828
Gabriel Bello <gabego@yahoo.com>
2023-01-23 20:01:12 -0500

    fix: first commit gabego

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,1 +1,2 @@
 const { getPagination, getPagingData } = require('../utils/sequelize-utils')
+const { getPagination, getPagingData } = require('../utils/sequelize-utils')const { getPagination, getPagingData } = require('../utils/sequelize-utils')
