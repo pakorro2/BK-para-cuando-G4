f7bea5667425925e3f6fedf576fd153c0b67556e
Erickbut <erickbutron1@gmail.com>
2023-01-27 16:04:23 -0400

    test: testing api

--- routes/models.routes.js
+++ routes/models.routes.js
@@ -1,15 +1,14 @@
 const express = require('express')
 const routesUsers = require('./users.routes')
 const routesAuth = require('./auth.routes')
+const swaggerUI = require('swagger-ui-express')
+const swaggerDoc = require('../libs/swagger.json')
 
 function routerModels(app) {
   const router = express.Router()
 
-  app.use('/api/v1', router)
-
   router.use('/users', routesUsers)
   router.use('/auth', routesAuth)
+  router.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDoc))
   // other models here
 }
-
-module.exports = routerModels
