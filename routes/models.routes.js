6b921ca35640022b1d1e89c2d00f86cf46534722
Pako Cárdenas <pakorro2@gmail.com>
2023-01-26 21:41:41 -0600

    docs: add swagger doc api in  routes.models and files swagger.json

--- routes/models.routes.js
+++ routes/models.routes.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const routesUsers = require('./users.routes')
 const routesAuth = require('./auth.routes')
+const swaggerUI = require('swagger-ui-express')
+const swaggerDoc = require('../libs/swagger.json')
 
 function routerModels(app) {
   const router = express.Router()
@@ -9,6 +11,7 @@ function routerModels(app) {
 
   router.use('/users', routesUsers)
   router.use('/auth', routesAuth)
+  router.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDoc))
   // other models here
 }
 
